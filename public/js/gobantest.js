5be53542d7c6810028cf1ef1aa5de01406499720
Shammah Chancellor <shammah.chancellor@gmail.com>
2013-10-04 18:56:08 -0400

    Removing all the eidogo code from this git repo, and working on it inside a YUI shifter module repository.

--- public/js/gobantest.js
+++ public/js/gobantest.js
@@ -3,30 +3,15 @@ YUI.GlobalConfig =
         filter: 'raw',
 	modules:
 	{
-	    channeltab: '/js/channeltab.js',
-	    usermodel: '/js/usermodel.js',
-	    goban: '/js/goban.js'
+	    //   goban: '/js/goban.js'
 	}
     };
 
-YUI().use('goban', function(Y)
-	{
-	    //var renderer = new Y.Goban({srcNode: '#goban'});
-
-	    var params = {},
-	    pairs = location.search ? location.search.substring(1).split(/&/) : [],
-	    i, keyval;
-	    for (i = 0; i < pairs.length; i++) {
-		keyval = pairs[i].split(/\=/);
-		params[keyval[0]] = keyval[1];
-	    }
-	    window.player = new eidogo.Player({
-		container:       "player-container",
-		renderer:        Y.Goban,
-		//theme:           "compact",
-		sgfUrl:          "sgf/" + (params.sgf ? params.sgf : "example.sgf")
-		//enableShortcuts: true
-	    }); 
-
-	    //renderer.player = window.player;
-	});
+YUI({ filter: 'debug' }).use('eidigo-player', 'eidigo-graphicrenderer', function(Y)
+			     {
+				 window.player = new Y.Eidogo.Player({
+				     srcNode:       "#goban",
+				     renderer:        new Y.Eidogo.GraphicRenderer({srcNode: '#goban'}),
+				     sgfUrl:          "sgf/" + (params.sgf ? params.sgf : "example.sgf")
+				 }); 
+			     });
