39d7b0750934b6b9bea2189ac7618f62b61628fc
Side_Effect <mr.person2000@gmail.com>
2013-11-02 09:47:44 -0500

    added title class and added code to userinfo :3

--- source/user/userinfo.d
+++ source/user/userinfo.d
@@ -5,8 +5,50 @@ import std.array;
 
 import redis;
 
+enum RelationTypes
+{
+	None = 0,
+	Friend = 1,
+	Blocked = 2
+}
+
 class UserInfo
 {
+	private int id;
+	private string username;
+	private int titleId;
+	private int rating;
+	private RelationTypes[string] relations;
 
+	@property public int Id()
+	{
+		return id;
+	}
 
+	@property public string Username()
+	{
+		return username;
+	}
+
+	@property public int TitleId()
+	{
+		return titleId;
+	}
+
+	@property public int Rating()
+	{
+		return rating;
+	}
+
+	public RelationTypes getRelation(string username)
+	{
+		auto friendEntry = username in relations;
+		if(friendEntry) return *friendEntry; 
+		else return 0;
+	}
+
+	public void setRelation(string username, RelationTypes type)
+	{
+		this.relations[username] = type;
+	}
 }
