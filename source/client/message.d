61b0e7f779318bbd680cdd2a88f2527f167ecf25
Shammah Chancellor <shammah.chancellor@gmail.com>
2013-11-02 11:24:37 -0400

    Fixed readArg, and added a unit test.

--- source/client/message.d
+++ source/client/message.d
@@ -1,48 +1,8 @@
 import std.conv;
 
-struct Message(char quote = '\"', char escape = '\\', char delimister=' ')
+struct Message
 {
   string[] args;
 
-  string readArg(string msg, ref int i = 0)
-  {
-    int argStart = 0;
-    bool quoted = false;
-    
-    int startIndex = i;
-    Appender!(string) output;
-    
-    for( ; i < msg.length; i++ )
-      {
-        switch( msg[index] )
-          {
-          case escape:
-            output.put(msg[startindex..i]);
-            startIndex = ++i; //Skip the next character
-            break;
-          case quote:
-            quoted = !quoted;
-            if( quoted ) {
-              startIndex = i + 1; //We don't want to read the initial quote.
-              continue;
-            } else {
-              goto case ' '; //Explicite fall through
-            }
-          case delimiter:
-            if( ! quoted ) {
-              if( output.data.length == 0) {
-                return msg[startIndex..i];
-              } else {
-                output.put(sgfData[startIndex..i]);
-                return output.data;
-              }
-            }
-            break;
-          default:
-            continue; 
-          }
-      }
 
-    return "";
-  }
 }
---
+++ source/util/util.d
@@ -0,0 +1,70 @@
+module util.util;
+
+import std.array;
+/**
+ * Destructively read a argument, and return it.  May allocate if there are escape characters in the string
+ *
+ */
+string readArg(char quote = '\"', char escape = '\\', char delimiter = ' ')(ref string msg)
+{
+    bool quoted = false;
+    int startIndex = 0;
+    Appender!(string) output;
+
+    int i = 0;
+    for( ; i < msg.length; i++ )
+    {
+        switch( msg[i] )
+        {
+            case escape:
+                output.put(msg[startIndex..i]);
+                startIndex = ++i; //Skip the next character
+                break;
+            case quote:
+                quoted = !quoted;
+                if( quoted ) {
+                    startIndex = i + 1; //We don't want to read the initial quote.
+                    break;
+                } else {
+                    goto case delimiter; //Explicite fall through
+                }
+            case delimiter:
+                if( ! quoted ) {
+                    goto end;
+                }
+                break;
+            default:
+                continue; 
+        }
+    }
+
+end:
+    auto arg = msg[startIndex..i];
+    if( i+1 < msg.length )
+        msg = msg[i+1..$]; //remove the delimiter
+    else
+        msg = msg[$..$];
+
+    if( output.data.length == 0) {
+        return arg;
+    } else {
+        output.put(arg);
+        return output.data;
+    }
+
+    assert(false, "We shouldn't be here");
+}
+
+alias readAll = readArg!('"', '\\', 0x00);
+
+unittest {
+    import std.stdio;
+    auto msg = "TELL \"Side Effect\" \"Hello how are you?";
+    auto arg = msg.readArg();
+    assert( arg == "TELL", "First arg wrong:" ~arg ~ " " ~ msg);
+    arg = msg.readArg(); 
+    assert( arg == "Side Effect", "Second arg wrong: " ~arg ~ " " ~ msg);
+    arg = msg.readAll(); 
+    assert( arg == "Hello how are you?", "Third arg wrong: " ~arg ~ " " ~ msg);
+
+}
